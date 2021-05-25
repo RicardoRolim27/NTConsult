5d87a150d65f44d886c13838143aaa4118c694b0
ricardo <ricardorolim27@gmail.com>
2021-05-25 01:05:53 -0300

    Update AssociadoRepository.java

--- TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/repositories/AssociadoRepository.java
+++ TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/repositories/AssociadoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ntconsult.votacaoPauta.entities.Associado;
 
+@Repository
 public interface AssociadoRepository extends JpaRepository<Associado, Long> {
 	List<Associado> findAllByOrderByNameAsc();
 }
