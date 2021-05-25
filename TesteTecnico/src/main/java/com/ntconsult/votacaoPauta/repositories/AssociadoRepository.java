630348fccf5417c9d1e6f7373b1260b40c5b67b7
ricardo <ricardorolim27@gmail.com>
2021-05-25 18:04:14 -0300

    Update AssociadoRepository.java

--- TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/repositories/AssociadoRepository.java
+++ TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/repositories/AssociadoRepository.java
@@ -3,6 +3,7 @@ package com.ntconsult.votacaoPauta.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ntconsult.votacaoPauta.entities.Associado;
 
