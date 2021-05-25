3692978e84148686985aca7306f31d1a5cbfc781
ricardo <ricardorolim27@gmail.com>
2021-05-24 23:58:03 -0300

    Implementação Interface AssociadoRepository

---
+++ TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/repositories/AssociadoRepository.java
@@ -0,0 +1,11 @@
+package com.ntconsult.votacaoPauta.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ntconsult.votacaoPauta.entities.Associado;
+
+public interface AssociadoRepository extends JpaRepository<Associado, Long> {
+	List<Associado> findAllByOrderByNameAsc();
+}
