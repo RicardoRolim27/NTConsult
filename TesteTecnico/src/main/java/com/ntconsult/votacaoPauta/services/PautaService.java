3424aaf05932d05fe388c1ca9117e2acd30478e4
ricardo <ricardorolim27@gmail.com>
2021-05-25 21:23:05 -0300

    Update PautaService.java

--- TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/services/PautaService.java
+++ TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/services/PautaService.java
@@ -7,12 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ntconsult.votacaoPauta.dto.AssociadoDTO;
 import com.ntconsult.votacaoPauta.dto.PautaDTO;
-import com.ntconsult.votacaoPauta.entities.Associado;
+import com.ntconsult.votacaoPauta.dto.VotoDTO;
 import com.ntconsult.votacaoPauta.entities.Pauta;
-import com.ntconsult.votacaoPauta.repositories.AssociadoRepository;
+import com.ntconsult.votacaoPauta.entities.Voto;
 import com.ntconsult.votacaoPauta.repositories.PautaRepository;
+import com.ntconsult.votacaoPauta.repositories.VotoRepository;
 
 @Service
 public class PautaService {
@@ -21,7 +21,7 @@ public class PautaService {
 	private PautaRepository pautaRepository;
 	
 	@Autowired
-	private AssociadoRepository associadoRepo;  
+	private VotoRepository votoRepo;  
 	
 	public List<PautaDTO> findAll(){
 		List<Pauta> list = pautaRepository.findAllByOrderByDescricaoAsc();
@@ -33,11 +33,11 @@ public class PautaService {
 	public PautaDTO insert(PautaDTO dto) {
 		Pauta pauta = new Pauta(null, dto.getDescricao());
 		
-		for(AssociadoDTO p : dto.getAssociados()) {
+		for(VotoDTO p : dto.getVotos()) {
 			@SuppressWarnings("deprecation")
-			Associado associado = associadoRepo.getOne(p.getId());
+			Voto voto = votoRepo.getOne(p.getId());
 			
-			pauta.getAssociados().add(associado);
+			pauta.getVotos().add(voto);
 			
 		}
 		
