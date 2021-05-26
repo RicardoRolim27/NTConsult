d5a14624d1b437acc16d52de6aa74d39e352fe15
ricardo <ricardorolim27@gmail.com>
2021-05-25 22:57:59 -0300

    Update Pauta.java

--- TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/entities/Pauta.java
+++ TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/entities/Pauta.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,7 +27,7 @@ public class Pauta implements Serializable {
 	
 	private String descricao;
 	
-	@OneToMany
+	@OneToMany(fetch = FetchType.LAZY)
 	@JoinTable(name = "pauta_voto",
 		joinColumns = @JoinColumn(name = "pauta_id"),
 		inverseJoinColumns = @JoinColumn(name = "voto_id"))
