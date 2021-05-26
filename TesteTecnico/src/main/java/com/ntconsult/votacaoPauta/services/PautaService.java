c20cc44b9f0add28780a78e0a6b2ef3ab62482ce
ricardo <ricardorolim27@gmail.com>
2021-05-26 00:17:39 -0300

    Update PautaService.java

--- TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/services/PautaService.java
+++ TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/services/PautaService.java
@@ -1,6 +1,7 @@
 package com.ntconsult.votacaoPauta.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,38 +9,32 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ntconsult.votacaoPauta.dto.PautaDTO;
-import com.ntconsult.votacaoPauta.dto.VotoDTO;
 import com.ntconsult.votacaoPauta.entities.Pauta;
-import com.ntconsult.votacaoPauta.entities.Voto;
 import com.ntconsult.votacaoPauta.repositories.PautaRepository;
-import com.ntconsult.votacaoPauta.repositories.VotoRepository;
+
 
 @Service
 public class PautaService {
 	
 	@Autowired
 	private PautaRepository pautaRepository;
-	
-	@Autowired
-	private VotoRepository votoRepo;  
-	
+	  
+
 	public List<PautaDTO> findAll(){
 		List<Pauta> list = pautaRepository.findAllByOrderByDescricaoAsc();
 		
 		return list.stream().map(x -> new PautaDTO(x)).collect(Collectors.toList());
 	}
 	
+	public Optional<Pauta> findById(){
+		Optional<Pauta> list = pautaRepository.findById(1l);
+		
+		return list;
+	}
+	
 	@Transactional
 	public PautaDTO insert(PautaDTO dto) {
 		Pauta pauta = new Pauta(null, dto.getDescricao());
-		
-		for(VotoDTO p : dto.getVotos()) {
-			@SuppressWarnings("deprecation")
-			Voto voto = votoRepo.getOne(p.getId());
-			
-			pauta.getVotos().add(voto);
-			
-		}
 		
 		pauta = pautaRepository.save(pauta);
 		
