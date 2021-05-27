0f78fe754e2b88ca88fabad96b8e612c74d8eedf
ricardo <ricardorolim27@gmail.com>
2021-05-26 23:43:27 -0300

    Update SessaoService.java

--- TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/services/SessaoService.java
+++ TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/services/SessaoService.java
@@ -1,11 +1,14 @@
 package com.ntconsult.votacaoPauta.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ntconsult.votacaoPauta.dto.PautaDTO;
 import com.ntconsult.votacaoPauta.dto.SessaoDTO;
 import com.ntconsult.votacaoPauta.entities.Pauta;
 import com.ntconsult.votacaoPauta.entities.Sessao;
@@ -28,8 +31,12 @@ public class SessaoService {
 		
 		sessao = sessaoRepo.save(sessao);
 		
-		
 		return new SessaoDTO(sessao);
+	}
+
+	public List<SessaoDTO> GetAll() {
+		List<Sessao> list = sessaoRepo.findAll();
+		return list.stream().map(x -> new SessaoDTO(x)).collect(Collectors.toList());
 	}
 
 }
