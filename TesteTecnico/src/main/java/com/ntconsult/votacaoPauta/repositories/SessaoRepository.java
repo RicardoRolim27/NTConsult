c7c80147d23583cb8564eea786f931daba10998b
ricardo <ricardorolim27@gmail.com>
2021-05-26 23:43:22 -0300

    Update SessaoRepository.java

--- TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/repositories/SessaoRepository.java
+++ TesteTecnico/src/main/java/com/ntconsult/votacaoPauta/repositories/SessaoRepository.java
@@ -1,5 +1,6 @@
 package com.ntconsult.votacaoPauta.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,8 @@ import com.ntconsult.votacaoPauta.entities.Sessao;
 
 @Repository
 public interface SessaoRepository extends JpaRepository<Sessao, Long>{
+	
+	List<Sessao> findAll();
 	
 	Optional<Sessao> findByPauta(Pauta pauta);
 
